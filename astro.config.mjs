5f106b1822873c605f1bdeca7606e7be820d46c4
denilson <denilsonfb21@gmail.com>
2026-02-18 13:50:54 -0300

    adição do config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://portifolio.manditecnologia.com.br',
+  base: '/', // Certifique-se de que não está '/portfolio'
+  output: 'static',
   integrations: [tailwind()],
-  base: '/portfolio/',
 });
